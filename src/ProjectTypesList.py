26a7f106295c5f28c4c0fb943917e3e0fa6a4892
Jan Hanson <jpchansondev@gmail.com>
2025-04-03 16:58:49 +0100

    Add listing of projects

---
+++ src/ProjectTypesList.py
@@ -0,0 +1,48 @@
+import pprint
+from ConfigManager import ConfMgr as Config
+from Config import ConfigStruct
+
+class ProjectTypesList:
+    """!
+    handle the listing of project types defined in the application config
+    """
+
+    NO_LANG = ConfigStruct(ERROR = "No projects found for language")
+
+    def run(self, args: dict):
+        """!
+        entry point to this command
+        @param args the dictionary of command line arguments provided at the
+               command line.
+        """
+        if args["--lang"] is None:
+            print("Listing all known projects for all known languages")
+            self.__print()
+        else:
+            self.__print(args["--lang"])
+
+
+    def __print(self, lang: str = None):
+        """!
+        if a language is provided handle the printing of all the projects
+        associated with that language. If no language is provided and this is
+        called with default arguments, list all projects for every language
+        that the application knows about.
+        @param lang a string representing the language to be listed. defaults to
+               None.
+        """
+        if lang is None:
+            for language,entries in vars(Config().projects).items():
+                print(f"- {language}")
+                self.__printProjects(entries)
+        else:
+            self.__printProjects(Config().projects.get(lang))
+
+    def __printProjects(self, language: ConfigStruct):
+        """!
+        print all the projects for a given language
+        @param language a ConfigStruct made up of the projects that are
+               applicable for a particular programming language.
+        """
+        for index, (project,source) in enumerate(vars(language).items()):
+            print(f"   {index+1}. {project} ({source})")
